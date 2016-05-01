ed13b4c9bed280d8f8f5f60b05a59eec501e4e7c
Sadip Giri <sadipgiri@Sadips-MacBook-Pro.local>
2016-05-01 17:32:28 -0400

    spawning enemies

---
+++ Battle Woods 9.44.27 PM/Assets/Scripts/Spawn.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+
+	public GameObject[] enemies;
+
+	public int numberOfEnemies;
+
+	private Vector3 spawnPoint;
+
+	void Update () {
+		enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+		numberOfEnemies = enemies.Length;
+
+
+		if(numberOfEnemies != 100){
+
+			InvokeRepeating ("spawnEnemy", 1, 5f);
+		}
+
+
+
+
+	}
+
+
+	void spawnEnemy()
+	{
+
+		spawnPoint.x = Random.Range (62,1901);
+		spawnPoint.y = 520f;
+		spawnPoint.z = Random.Range (1617,3100);
+
+		Instantiate (enemies[UnityEngine.Random.Range(0,enemies.Length - 1)],spawnPoint,Quaternion.identity);
+
+		CancelInvoke ();
+	}
+
+
+}
---
+++ Battle Woods 9.44.27 PM/Assets/Scripts/move.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+	public Transform track;
+	private float moveSpeed = 3;
+
+	void Update () {
+		float move = moveSpeed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, track.position, move);
+	}
+}

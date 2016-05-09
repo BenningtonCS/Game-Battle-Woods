0eff4e87e8b7997183f677687b025d09fe1c72cd
aksbhandari1 <akashBhandari@Akashs-MacBook-Pro.local>
2016-05-09 11:36:23 -0400

    enemy movement

---
+++ Battle Woods 9.44.27 PM/Assets/Scripts/move.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+
+	public Transform track;
+	private float moveSpeed = 3;
+	Animator enemyAnim;
+
+
+	void Start(){
+
+		//getting the animator component for the enemy
+		enemyAnim = GetComponent<Animator> ();
+	}
+
+	void Update () {
+
+		//creating a variable for moving the enemy after spawning from some point
+		float move = moveSpeed * Time.deltaTime;
+
+		//transforming the position of the spawn enemy to track the player
+		transform.position = Vector3.MoveTowards (transform.position, track.position, move);
+
+		//so animating the enemies so that they'd move as well as animate according to the player position
+		if (move != 0) {
+			enemyAnim.SetBool ("isWalking", true);
+		} else {
+			enemyAnim.SetBool ("isWalking", false);
+		}
+	
+	}
+}
